f491d1c6d232b410d8484379891ca6aa94793708
irisbaltus <150801536+irisbaltus@users.noreply.github.com>
2024-12-13 10:06:45 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,16 @@
 // JavaScript Document
-console.log("hi");
+// console.log("hi");
 
-let checkJustAnnounced = document.getElementById('justAnnounced').checked;
-let checkNextUp = document.getElementById('nextUp').checked;
+// let checkJustAnnounced = document.getElementById('justAnnounced').checked;
+// let checkNextUp = document.getElementById('nextUp').checked;
 
-let activeFilter = [];
-if(checkJustAnnounced){
-    activeFilter.push('justAnnounced');
-}
-if(checkNextUp){
-    activeFilter.push('nextUp');
-}
+// let activeFilter = [];
+// if(checkJustAnnounced){
+//     activeFilter.push('justAnnounced');
+// }
+// if(checkNextUp){
+//     activeFilter.push('nextUp');
+// }
 
 // function check(){
 //     let checkJustAnnounced = document.getElementById('justAnnounced').checked;
